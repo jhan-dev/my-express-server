c66d22b0f07b7415263db301ba41740f297bcdde
jhan-dev <jhan.technology@gmail.com>
2023-02-14 14:31:21 -0800

    Live updating node server w/ express using nodemon

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # my-express-server
 
 Configured package-json with $npm init
+
 Installed express with $npm install express
+
 Node server running on port 3000
+
+Installed npm packager nodemon with $npm install -g nodemon
+
+If not working, use:
+
+$sudo npm install -g nodemon
--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", function(req, res){
+    // console.log(request)
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at: jhan.technology@gmail.com")
+})
+
+app.get("/about", function(req, res){
+    res.send("Hi, my name is Joseph! I'm a Web Developer from Las Vegas!")
+})
+
 app.listen(3000, function(){
     console.log("Server started on port 3000")
 })
